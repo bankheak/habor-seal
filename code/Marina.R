e41c16ee5823cbc687ade9c9121883f721b71b94
Kyra Bankhead <bankheak@oregonstate.edu>
2022-12-15 14:51:19 -0800

    nothing

---
+++ code/Marina.R
@@ -0,0 +1,54 @@
+# Anthropogenic noise disturbance on harbor seals
+
+###########################################################################
+# Marina analysis
+###########################################################################
+
+# Set working directory here
+setwd("C:/Users/bankh/My_Repos/habor-seal/data")
+
+# Retrieve data
+full.mdata<-read.csv("full.mdata.csv")
+
+# Load packages
+require(ggplot2)
+
+###########################################################################
+# PART 1: Find distribution---------------------------------------------
+
+# Run pairwise cor between all independent variables
+## Cut-off is +/- 0.7
+cor.matrix<-cor(full.mdata[,c(2:4,6:7)]) 
+# Keep only large correlations in the same model
+cor.matrix[abs(cor.matrix)< 0.7]<-NA
+cor.matrix
+## No colilinearity
+
+# Check distribution
+ggplot(full.mdata, aes(x=seals)) + 
+  geom_histogram(aes(y=..density..), colour="black", fill="white")+
+  geom_density()+stat_density(alpha=.2,adjust = 1, fill="#FF6666")+xlab("Number of Seals Hauled-out")+ylab("Density")+theme(panel.background = element_blank())
+## Quasipoisson would be the best fit
+
+###########################################################################
+# PART 2: Run GLM and AICc ---------------------------------------------
+# Check GLM
+m.mod1<- glm(seals ~ 1, data = full.mdata, family = quasipoisson)
+summary(m.mod1) 
+m.mod2<- glm(seals ~ noise + month + tide + time, data = full.mdata, family = quasipoisson)
+summary(m.mod2) 
+m.mod3<- glm(seals ~ noise + month + time, data = full.mdata, family = quasipoisson)
+summary(m.mod3) 
+m.mod4<- glm(seals ~ noise + month, data = full.mdata, family = quasipoisson)
+summary(m.mod4) 
+
+# Create list
+m.mods<-list(m.mod4, m.mod4, m.mod4, m.mod4)
+
+## Find the AICc function
+source("../code/Functions.R")
+
+## Calculate AICc with glm of models
+AICc(m.mods) # Looks like site*noise + month + time are the best predictors
+
+summary(model3)
